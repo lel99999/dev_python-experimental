63cc7ea6c386fd8ef4ab6d8e1437466d6ef23c24
lel99999 <luan.le@cfpb.gov>
2022-11-10 23:58:47 -0500

    add files

---
+++ GUI/01_app_class.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+
+def center(win):
+    """
+    centers a tkinter window
+    :param win: the main window or Toplevel window to center
+    """
+    win.update_idletasks()
+    width = win.winfo_width()
+    frm_width = win.winfo_rootx() - win.winfo_x()
+    win_width = width + 2 * frm_width
+    height = win.winfo_height()
+    titlebar_height = win.winfo_rooty() - win.winfo_y()
+    win_height = height + titlebar_height + frm_width
+    x = win.winfo_screenwidth() // 2 - win_width // 2
+    y = win.winfo_screenheight() // 2 - win_height // 2
+    win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+    win.deiconify()
+
+class Root(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.label = tk.Label(self, text="App with Class", padx=5, pady=5)
+        self.label.pack()
+
+if __name__ == "__main__":
+    root = Root()
+#   center(root)
+    root.mainloop() 
