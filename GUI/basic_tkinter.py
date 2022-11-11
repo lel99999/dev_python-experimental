6603d398df440bd228b9ba1438e3be1ea7a4d39e
lel99999 <luan.le@cfpb.gov>
2022-11-10 23:37:05 -0500

    add gui files

---
+++ GUI/basic_tkinter.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+# create root window
+root = Tk()
+
+# set root window title and dimension
+root.title("Basic Window")
+root.geometry('400x300')
+
+#add widgets
+
+#execute
+root.mainloop()
